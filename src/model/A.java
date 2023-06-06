17237dc8979359e331f4a21c572e98d59670ae3d
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-06-06 23:45:34 +0530

    instance methods

--- src/model/A.java
+++ src/model/A.java
@@ -1,14 +1,24 @@
 package model;
 
 public class A {
-
     public static void main(String[] args) {
-        System.out.println("Main");
-        hello();
-        A.hello();
-    }
-    public static void hello () {
-        System.out.println("Hello");
+        System.out.println(A.x1);
+        A.x1 = 20;
+        System.out.println(A.x1);
+        
+//        System.out.println(new A().x2);
+       
+        A a1 = new A();
+        System.out.println(a1.x2);
+        a1.x2 = 50;
+        System.out.println(a1.x2);
+        
+        A a2 = new A();
+        System.out.println(a2.x2);
+        
     }
     
+    static int x1 = 10;
+    
+    int x2 = 10;
 }
